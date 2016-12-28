18e8e3b12883d1c0b1ddfb121aed09ecd36e5552
PaulBender <paulbender@live.com>
2016-12-28 14:28:37 -0500

    Bump to version v1.2

--- README.txt
+++ README.txt
@@ -1,6 +1,6 @@
 ## INFORMATION ##
 
-My Arcade (1.1) for MyBB 1.8
+My Arcade (1.2) for MyBB 1.8
 Created by: Starpaul20
 Copyright: ©2015
 License: GPL
--- inc/plugins/myarcade.php
+++ inc/plugins/myarcade.php
@@ -91,7 +91,7 @@ function myarcade_info()
 		"website"			=> "http://galaxiesrealm.com/index.php",
 		"author"			=> "Starpaul20",
 		"authorsite"		=> "http://galaxiesrealm.com/index.php",
-		"version"			=> "1.1",
+		"version"			=> "1.2",
 		"codename"			=> "myarcade",
 		"compatibility"		=> "18*"
 	);
